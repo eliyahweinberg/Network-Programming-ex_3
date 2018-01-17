ca47824e9448c8bdafa05a811d365b8101b40e1f
Eliyah Weinberg <eliyahweinberg@gmail.com>
2018-01-17 23:20:15 +0200

    added function for headers building

--- ex_3/server.c
+++ ex_3/server.c
@@ -22,19 +22,31 @@
 #define USAGE "Usage: server <port> <pool-size> <max-number-of-request>\n"
 #define R_EOL "\r\n"
 #define R_HTTP "HTTP/1.1 "
-#define R_SERVER "Server: webserver/1.0"
+#define R_SERVER "Server: webserver/1.1"
 #define R_DATE "Date: "
 #define R_LOC "Location: "
 #define R_CTYPE "Content-Type: "
+#define R_DEF_CTYPE "Content-Type: text/html"
 #define R_CLEN "Content-Length: "
 #define R_LS_MODIFIED "Last-Modified: "
 #define R_CONNECTION "Connection: close"
+#define R_
 
 #define TIMEBUF 128
 #define SUCCESS 0
 #define FAILURE -1
 #define TRUE 1
 #define FALSE 0
+#define CONECTION_CLOSED 1
+
+#define OK 200
+#define FOUND 302
+#define BAD_REQUEST 400
+#define FORBIDDEN 403
+#define NOT_FOUND 404
+#define INTERNAL_ERROR 500
+#define NOT_SUPPORTED 501
+
 
 typedef struct _attributes {
     threadpool* pool;
@@ -45,7 +57,14 @@ typedef struct _attributes {
     char timebuf[TIMEBUF];
 }server_attribs;
 
-
+typedef struct _responce_attributes {
+    int response_headrs_len;
+    int status;
+    int content_len;
+    char* last_modified;
+    char* path;
+    char* content_type;
+}response_attribs;
 
 //----------------------------------------------------------------------------//
 //--------------------------FUNCTION DECLARATION------------------------------//
@@ -62,47 +81,59 @@ void dealloc_resources(server_attribs* attribs);
 
 int service_client(void* args);
 
+int receive_request(int sock_fd, unsigned char** buff, int* req_len);
+
+unsigned char* response_500(int* response_len);
+
+unsigned char* response_400(int* response_len);
+
+char* build_resp_head(response_attribs* resp);
+
+int send_responce(int sock_fd, int status, unsigned char* req, int request_len);
 //----------------------------------------------------------------------------//
 //------------------------------M A I N---------------------------------------//
 //----------------------------------------------------------------------------//
 int main(int argc, const char * argv[]) {
-    int sock_fd;
-    int newsock_fd;
-    struct sockaddr cli_addr;
-    socklen_t clilen;
-    /*checking correct usage command*/
-    if (argc != 3) {
-        printf(USAGE);
-        return FAILURE;
-    }
+//    int sock_fd;
+//    int newsock_fd;
+//    struct sockaddr cli_addr;
+//    socklen_t clilen;
+//    /*checking correct usage command*/
+//    if (argc != 4) {
+//        printf(USAGE);
+//        return FAILURE;
+//    }
+//
+//    server_attribs* attribs = init_attribs(argc, argv);
+//    if (!attribs)
+//        return FAILURE;
+//
+//    sock_fd = init_server(attribs->port);
+//    if (sock_fd == FAILURE){
+//        dealloc_resources(attribs);
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    while (attribs->curr_req_num < attribs->max_requests_num) {
+//        newsock_fd = accept(sock_fd, (struct sockaddr*)&cli_addr, &clilen);
+//
+//        if (newsock_fd < 0){
+//            perror("Error on accept");
+//            continue;
+//        }
+//
+//        attribs->clients[attribs->curr_req_num] = newsock_fd;
+//
+//        service_client(& attribs->clients[attribs->curr_req_num]);
+//
+//        attribs->curr_req_num++;
+//    }
+//
+//
+//    close(sock_fd);
+//    dealloc_resources(attribs);
     
-    server_attribs* attribs = init_attribs(argc, argv);
-    if (!attribs)
-        return FAILURE;
-    
-    sock_fd = init_server(attribs->port);
-    if (sock_fd == FAILURE){
-        dealloc_resources(attribs);
-        exit(EXIT_FAILURE);
-    }
-    
-    while (attribs->curr_req_num < attribs->max_requests_num) {
-        newsock_fd = accept(sock_fd, (struct sockaddr*)&cli_addr, &clilen);
-        
-        if (newsock_fd < 0){
-            perror("Error on accept");
-            continue;
-        }
-        
-        attribs->clients[attribs->curr_req_num] = newsock_fd;
-        
-
-        attribs->curr_req_num++;
-    }
-    
-    
-    close(sock_fd);
-    dealloc_resources(attribs);
+   
     return 0;
 }
 
@@ -207,6 +238,227 @@ void dealloc_resources(server_attribs* attribs){
     free(attribs);
 }
 
+//----------------------------------------------------------------------------//
 int service_client(void* args){
-    
+    int cli_sock_fd = *(int*)args;
+    int status;
+    int req_len;
+    unsigned char* request;
+    status = receive_request(cli_sock_fd, &request, &req_len);
+    if (status != CONECTION_CLOSED)
+        send_responce(cli_sock_fd, status, request, req_len);
+
+    close(cli_sock_fd);
+    return SUCCESS;
 }
+
+//----------------------------------------------------------------------------//
+int receive_request(int sock_fd, unsigned char** buff, int* req_len){
+    ssize_t rc;
+    const int REQUEST_LINE = 4000;
+    int offset = 0;
+    int r_found = FALSE;
+    int n_found = FALSE;
+    int i;
+    *req_len = 0;
+    unsigned char* request = (unsigned char*)malloc(REQUEST_LINE*sizeof(char));
+    if (!request)
+        return FAILURE;
+    
+    memset(request, '\0', REQUEST_LINE);
+    while (TRUE) {
+        rc = read(sock_fd, request+offset, REQUEST_LINE-offset);
+        if (rc < 0){
+            free(request);
+            return INTERNAL_ERROR;
+        } else if (rc == 0){
+            free(request);
+            return CONECTION_CLOSED;
+        }
+        offset += rc;
+        /*Checking if EOF terminator received
+         *loop running only for new data that received*/
+        for (i = (offset == 0 ? offset : offset-(int)rc); i<rc; i++) {
+            if (request[i] == '\r')
+                r_found = TRUE;
+            else if (request[i] == '\n' && i>0 && request[i-1] == '\r')
+                n_found = TRUE;
+            
+            if (r_found && n_found){
+                *req_len = i;
+                break; //EOF terminator received
+            }
+        }
+        
+        /*EOF terminator received or maximal lenght of the line reached*/
+        if (*req_len || offset == REQUEST_LINE)
+            break;
+    }
+    
+    if (!*req_len) {
+        free(request);
+        return BAD_REQUEST;
+    }
+    *buff = (unsigned char*)malloc(((*req_len)+1)*sizeof(char));
+    if (!(*buff)){
+        free(request);
+        return INTERNAL_ERROR;
+    }
+    
+    for (i=0; i<*req_len; i++)
+        *buff[i] = request[i];
+    
+    *buff[*req_len] = '\0';
+
+    free(request);
+    return SUCCESS;
+}
+
+//----------------------------------------------------------------------------//
+int send_responce(int sock_fd, int status, unsigned char* req, int request_len){
+    unsigned char* response = NULL;
+    int response_len = 0;
+    
+    if (status == INTERNAL_ERROR)
+        response = response_500(&response_len);
+    else if (status == BAD_REQUEST)
+        response = response_400(&response_len);
+    
+    
+    return SUCCESS;
+}
+
+unsigned char* response_500(int* response_len){
+    return NULL;
+}
+
+unsigned char* response_400(int* response_len){
+    return NULL;
+}
+
+char* build_resp_head(response_attribs* resp){
+    int headers_len = 0;
+    int i;
+    int temp_c_len = resp->content_len;
+    char timebuf [TIMEBUF];
+    char* str_cont_len;
+    char* phrase = NULL;
+    char str_status [4];
+    char* response;
+    memset(timebuf, '\0', TIMEBUF);
+    get_time(timebuf);
+    headers_len += strlen(R_HTTP);
+    headers_len += 3;//status lenght
+    headers_len += (strlen(R_EOL)*6);
+    headers_len += strlen(R_SERVER);
+    headers_len += strlen(timebuf) + strlen(R_DATE);
+    if (resp->status == OK && resp->content_type){
+        headers_len += strlen(R_CTYPE);
+        headers_len += strlen(R_EOL);
+    }
+    else if (resp->status != OK){
+        headers_len += strlen(R_DEF_CTYPE);
+        headers_len += strlen(R_EOL);
+    }
+    headers_len += strlen(R_CLEN);
+    headers_len += strlen(R_CONNECTION);
+    
+    /*count content lenght digits numner*/
+    for (i=0; temp_c_len != 0; i++)
+        temp_c_len /= 10;
+    
+    str_cont_len = (char*)malloc(sizeof(char)*(i+1));
+    sprintf(str_cont_len, "%d", resp->content_len);
+    sprintf(str_status, "%d", resp->status);
+    headers_len += strlen(str_cont_len);
+    
+    switch (resp->status) {
+        case OK:
+            phrase = strdup(" OK");
+            headers_len += strlen(R_LS_MODIFIED);
+            headers_len += strlen(resp->last_modified);
+            headers_len += strlen(R_EOL);
+            if (resp->content_type)
+                headers_len += strlen(resp->content_type);
+            break;
+            
+        case FOUND:
+            phrase = strdup(" Found");
+            headers_len += strlen(R_LOC);
+            headers_len += strlen(R_EOL);
+            headers_len += strlen(resp->path);
+            break;
+            
+        case BAD_REQUEST:
+            phrase = strdup(" Bad Request");
+            break;
+            
+        case FORBIDDEN:
+            phrase = strdup(" Forbidden");
+            break;
+            
+        case NOT_FOUND:
+            phrase = strdup(" Not Found");
+            break;
+            
+        case INTERNAL_ERROR:
+            phrase = strdup(" Internal Server Error");
+            break;
+        
+        case NOT_SUPPORTED:
+            phrase = strdup(" Not supported");
+            break;
+    }
+    headers_len += strlen(phrase);
+    
+    resp->response_headrs_len = headers_len;
+    /*building response*/
+    response = (char*)malloc(sizeof(char)*(headers_len+1));
+    
+    strcpy(response, R_HTTP);
+    strcat(response, str_status);
+    strcat(response, phrase);
+    strcat(response, R_EOL);
+    strcat(response, R_SERVER);
+    strcat(response, R_EOL);
+    strcat(response, R_DATE);
+    strcat(response, timebuf);
+    strcat(response, R_EOL);
+    if (resp->status == FOUND) {
+        strcat(response, R_LOC);
+        strcat(response, resp->path);
+        strcat(response, R_EOL);
+    }
+    if (resp->status != OK){
+        strcat(response, R_DEF_CTYPE);
+        strcat(response, R_EOL);
+    } else if (resp->content_type){
+        strcat(response, R_CTYPE);
+        strcat(response, resp->content_type);
+        strcat(response, R_EOL);
+    }
+    strcat(response, R_CLEN);
+    strcat(response, str_cont_len);
+    strcat(response, R_EOL);
+    if (resp->status == OK){
+        strcat(response, R_LS_MODIFIED);
+        strcat(response, resp->last_modified);
+        strcat(response, R_EOL);
+    }
+    strcat(response, R_CONNECTION);
+    strcat(response, R_EOL);
+    strcat(response, R_EOL);
+    
+//    if (resp->content_type)
+//        free(resp->content_type);
+//    if (resp->last_modified)
+//        free(resp->last_modified);
+//    if (resp->path)
+//        free(resp->path);
+    
+    free(str_cont_len);
+    free(phrase);
+    
+    return response;
+}
+
